c55b25cb4b9ab229c9e043cc46dd94cbcddb0262
Rafael Poffo Filho <rafaelpoffofilho@gmail.com>
2023-08-17 18:38:12 -0300

    first commit

---
+++ mobile/src/index.tsx
@@ -0,0 +1,25 @@
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import { Loading } from './components/Loading';
+
+import { Pools } from './screens/Pools';
+// import { SignIn } from './screens/SignIn';
+
+export function Main() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return <Pools />;
+}
